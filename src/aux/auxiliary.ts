3b9cdd21634ddca300e2ba46cf1359fbfa6ed3b4
ninoseki <zgok0079@gmail.com>
2018-06-28 08:41:48 +0900

    Refactored regexs
    - Add more tlds for domain matching
    - replaced /mg to /ig

--- src/aux/auxiliary.ts
+++ src/aux/auxiliary.ts
@@ -1,6 +1,6 @@
 
 export function dedup(s: string): string {
-  const list = s.split(/\s+/mg);
+  const list = s.split(/\s+/ig);
   const uniq = list.filter((x, i, self) => {
     return self.indexOf(x) === i;
   });
@@ -8,10 +8,10 @@ export function dedup(s: string): string {
 }
 
 export function clean(s: string): string {
-  return s.replace(/\[\.\]/mg, '.').
-    replace(/\[\./mg, '.').
-    replace(/\.\]/mg, '.').
-    replace(/\(\.\)/mg, '.').
-    replace(/\(\./mg, '.').
-    replace(/\.\)/mg, '.');
+  return s.replace(/\[\.\]/ig, '.').
+    replace(/\[\./ig, '.').
+    replace(/\.\]/ig, '.').
+    replace(/\(\.\)/ig, '.').
+    replace(/\(\./ig, '.').
+    replace(/\.\)/ig, '.');
 }
