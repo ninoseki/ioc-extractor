326f796f2ff583ec6d169f1a3552e5d9549b7c54
Manabu Niseki <manabu.niseki@gmail.com>
2019-05-31 10:31:18 +0900

    chore: switch from mocha/chai to jest

--- src/spec/regexs.spec.ts
+++ src/spec/regexs.spec.ts
@@ -1,5 +1,3 @@
-import { expect } from "chai";
-import "mocha";
 import {
   asnRegex,
   btcRegex,
@@ -37,14 +35,14 @@ import { isIPv4 } from "net";
 
 describe("isMD5", () => {
   it("checks whther a given value is MD5 or not", () => {
-    expect(isMD5("874058e8d8582bf85c115ce319c5b0af")).to.equal(true);
-    expect(isMD5("874058e8d8582bf85c115ce319c5b0a")).to.equal(false);
+    expect(isMD5("874058e8d8582bf85c115ce319c5b0af")).toBe(true);
+    expect(isMD5("874058e8d8582bf85c115ce319c5b0a")).toBe(false);
   });
 });
 
 describe("isSHA1", () => {
   it("checks whther a given value is SHA256 or not", () => {
-    expect(isSHA1("a89ca560e2f0f3b081a3e7d0d91a2bcd4e2bdadb")).to.equal(true);
+    expect(isSHA1("a89ca560e2f0f3b081a3e7d0d91a2bcd4e2bdadb")).toBe(true);
   });
 });
 
@@ -54,7 +52,7 @@ describe("isSHA256", () => {
       isSHA256(
         "9002a1d301adf6e0426af36a40c3c1b33db7891f3f7d93f1cf6e73fdbce0da1b"
       )
-    ).to.equal(true);
+    ).toBe(true);
   });
 });
 
@@ -64,7 +62,7 @@ describe("isSHA512", () => {
       isSHA512(
         "5c815af2e56a9bcfa0751dc097321de9fbb074603d2dd12c00a4ea45ebc819310b9bc871872b27b20366b71e78f520b908f1ea4bd6458cedb748e5ea8a510f51"
       )
-    ).to.equal(true);
+    ).toBe(true);
   });
 });
 
@@ -74,60 +72,60 @@ describe("isSSDEEP", () => {
       isSSDEEP(
         "1536:gQA4ws6RTPdn1Jl19DzTIKu6VnGUNs+9Kf8r3Vf/WcnfYhLrJBew:uiEo/"
       )
-    ).to.equal(true);
+    ).toBe(true);
   });
 });
 
 describe("isASN", () => {
   it("checks whther a given value is ANS or not", () => {
-    expect(isASN("ASN13335")).to.equal(true);
+    expect(isASN("ASN13335")).toBe(true);
   });
 });
 
 describe("isDomain", () => {
   it("checks whther a given value is Domain or not", () => {
-    expect(isDomain("example.com")).to.equal(true);
+    expect(isDomain("example.com")).toBe(true);
   });
 });
 
 describe("isIPv4", () => {
   it("checks whther a given value is IPv4 or not", () => {
-    expect(isIPv4("8.8.8.8")).to.equal(true);
+    expect(isIPv4("8.8.8.8")).toBe(true);
   });
 });
 
 describe("isIPv6", () => {
   it("checks whther a given value is IPv6 or not", () => {
-    expect(isIPv6("fe80::d544:e71:3c17:7710%11")).to.equal(true);
+    expect(isIPv6("fe80::d544:e71:3c17:7710%11")).toBe(true);
   });
 });
 
 describe("isEmail", () => {
   it("checks whther a given value is Email or not", () => {
-    expect(isEmail("test@test.com")).to.equal(true);
+    expect(isEmail("test@test.com")).toBe(true);
   });
 });
 
 describe("isURL", () => {
   it("checks whther a given value is URL or not", () => {
-    expect(isURL("https://www.example.com/foo/bar?baz=1")).to.equal(true);
-    expect(isURL("https://111.111.111.111/foo/bar?baz=1")).to.equal(true);
+    expect(isURL("https://www.example.com/foo/bar?baz=1")).toBe(true);
+    expect(isURL("https://111.111.111.111/foo/bar?baz=1")).toBe(true);
   });
 });
 
 describe("isCVE", () => {
   it("checks whther a given value is CVE or not", () => {
-    expect(isCVE("CVE-1800-0000")).to.equal(false);
-    expect(isCVE("CVE-2016-0000")).to.equal(true);
-    expect(isCVE("CVE-2100-0000")).to.equal(false);
-    expect(isCVE("CVE-2016-00000")).to.equal(true);
-    expect(isCVE("CVE-20100-0000")).to.equal(false);
+    expect(isCVE("CVE-1800-0000")).toBe(false);
+    expect(isCVE("CVE-2016-0000")).toBe(true);
+    expect(isCVE("CVE-2100-0000")).toBe(false);
+    expect(isCVE("CVE-2016-00000")).toBe(true);
+    expect(isCVE("CVE-20100-0000")).toBe(false);
   });
 });
 
 describe("isBTC", () => {
   it("checks whther a given value is BTC or not", () => {
-    expect(isBTC("1J6PYEzr4CUoGbnXrELyHszoTSz3wCsCaj")).to.equal(true);
+    expect(isBTC("1J6PYEzr4CUoGbnXrELyHszoTSz3wCsCaj")).toBe(true);
   });
 });
 
@@ -137,20 +135,20 @@ describe("isXMR", () => {
       isXMR(
         "48Fki6gnEN1QaiWNcsm8dVfX2JMg8xmjiQvuKpcdUD9rQH8WU4AXj9HKAF5AdnhKPSPLzTV7CX1Ks25BWrDeLnHuEFmhRxV"
       )
-    ).to.equal(true);
+    ).toBe(true);
   });
 });
 
 describe("isGATrackID", () => {
   it("checks whther a given value is GATrackID or not", () => {
-    expect(isGATrackID("UA-26296840-4")).to.equal(true);
-    expect(isGATrackID("UA-26296840")).to.equal(true);
+    expect(isGATrackID("UA-26296840-4")).toBe(true);
+    expect(isGATrackID("UA-26296840")).toBe(true);
   });
 });
 
 describe("isGATrackID", () => {
   it("checks whther a given value is GAPubID or not", () => {
-    expect(isGAPubID("pub-9107453047749393")).to.equal(true);
+    expect(isGAPubID("pub-9107453047749393")).toBe(true);
   });
 });
 
@@ -159,24 +157,24 @@ describe("hashRegexs", () => {
     const input =
       "ad535056bf6318d5faf47d3abcc2b902\nhoge\na9c08de3a3c0cf353422f0f69f0e4e26";
     const matches = input.match(md5Regex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("ad535056bf6318d5faf47d3abcc2b902");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("ad535056bf6318d5faf47d3abcc2b902");
   });
 
   it("should match with all sha1 values in the input", () => {
     const input =
       "a89ca560e2f0f3b081a3e7d0d91a2bcd4e2bdadb\n1a229aee30e0a4472772a2a96bc1dc75c8cd8568";
     const matches = input.match(sha1Regex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("a89ca560e2f0f3b081a3e7d0d91a2bcd4e2bdadb");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("a89ca560e2f0f3b081a3e7d0d91a2bcd4e2bdadb");
   });
 
   it("should match with all sha256 values in the input", () => {
     const input =
       "9002a1d301adf6e0426af36a40c3c1b33db7891f3f7d93f1cf6e73fdbce0da1b\nbb89bfc8144ef401b671d4f9f34b296055e30bfd5c400fbef794f5c28b84ad0d";
     const matches = input.match(sha256Regex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal(
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe(
       "9002a1d301adf6e0426af36a40c3c1b33db7891f3f7d93f1cf6e73fdbce0da1b"
     );
   });
@@ -185,8 +183,8 @@ describe("hashRegexs", () => {
     const input =
       "5c815af2e56a9bcfa0751dc097321de9fbb074603d2dd12c00a4ea45ebc819310b9bc871872b27b20366b71e78f520b908f1ea4bd6458cedb748e5ea8a510f51\nf4d8b2044fbe715ccf04fbcfe3e04824a883236285bbcaec816ed520a1ae49ea0bdc3b8352e48c496b0910267e0037a0ca7c47a9f91f7f1b4f8e8eb4b6769717";
     const matches = input.match(sha512Regex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal(
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe(
       "5c815af2e56a9bcfa0751dc097321de9fbb074603d2dd12c00a4ea45ebc819310b9bc871872b27b20366b71e78f520b908f1ea4bd6458cedb748e5ea8a510f51"
     );
   });
@@ -195,8 +193,8 @@ describe("hashRegexs", () => {
     const input =
       "1536:gQA4ws6RTPdn1Jl19DzTIKu6VnGUNs+9Kf8r3Vf/WcnfYhLrJBew:uiEo/\n3:AXGBicFlgVNhBGcL6wCrFQEv:AXGHsNhxLsr2C";
     const matches = input.match(ssdeepRegex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal(
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe(
       "1536:gQA4ws6RTPdn1Jl19DzTIKu6VnGUNs+9Kf8r3Vf/WcnfYhLrJBew:uiEo/"
     );
   });
@@ -206,48 +204,48 @@ describe("networkRegexes", () => {
   it("should match with all asn values", () => {
     const input = "ASN13335 AS13334";
     const matches = input.match(asnRegex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("ASN13335");
-    expect(matches[1]).to.equal("AS13334");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("ASN13335");
+    expect(matches[1]).toBe("AS13334");
   });
 
   it("should match with all domain values", () => {
     const input = "test.co.jp\ngitlab.com\ntest.exe\ndev.test.co.jp";
     const matches = input.match(domainRegex)!;
-    expect(matches.length).to.equal(3);
-    expect(matches[0]).to.equal("test.co.jp");
-    expect(matches[1]).to.equal("gitlab.com");
-    expect(matches[2]).to.equal("dev.test.co.jp");
+    expect(matches.length).toBe(3);
+    expect(matches[0]).toBe("test.co.jp");
+    expect(matches[1]).toBe("gitlab.com");
+    expect(matches[2]).toBe("dev.test.co.jp");
   });
 
   it("should match with all email values", () => {
     const input = "test@test.co.jp\ntest@test.com\nhoge@hoge";
     const matches = input.match(emailRegex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("test@test.co.jp");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("test@test.co.jp");
   });
 
   it("should match with all ipv4 values", () => {
     const input = "8.8.8.8\n127.0.0.1";
     const matches = input.match(ipv4Regex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("8.8.8.8");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("8.8.8.8");
   });
 
   it("should match with all ipv6 values", () => {
     const input =
       " 2001:0db8:85a3:0000:0000:8a2e:0370:7334\n fe80::d544:e71:3c17:7710%11";
     const matches = input.match(ipv6Regex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
   });
 
   it("should match with all url values", () => {
     const input =
       "https://test.co.jp\nhttps://google.com\nhttps://111.111.111.111/test.jsp\nwww.example.com";
     const matches = input.match(urlRegex)!;
-    expect(matches.length).to.equal(3);
-    expect(matches[0]).to.equal("https://test.co.jp");
+    expect(matches.length).toBe(3);
+    expect(matches[0]).toBe("https://test.co.jp");
   });
 });
 
@@ -255,8 +253,8 @@ describe("utilityRegexs", () => {
   it("should match with all CVE values in the input", () => {
     const input = "foo bar CVE-2000-0001 baz";
     const matches = input.match(cveRegex)!;
-    expect(matches.length).to.equal(1);
-    expect(matches[0]).to.equal("CVE-2000-0001");
+    expect(matches.length).toBe(1);
+    expect(matches[0]).toBe("CVE-2000-0001");
   });
 });
 
@@ -265,15 +263,15 @@ describe("cryptocurrenciesRegexs", () => {
     const input =
       "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa 3Gj9sY6PaBHdk44jktV7AXrktjMcDqnwV8 49VLRG6oXpBEHACpoTpNh23Y3fGxStciKq";
     const matches = input.match(btcRegex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa");
   });
   it("should match with all XMR addresses in the input", () => {
     const input =
       "42CujFXn1HHiwrGW3Wuh8TASmo94dv3J8DZneS5NqBhaJVNi4qK32Zj3rgcDWsrxznP1qtjJFBKtHQCcbSCY996wMHHfvhw\n42UE32EZxHAWejXi3nQ3wpYGncQnbCw6LCMh8PkcFTn6XzQUUDco2pGSpR6AJHFK1jL8tYNYJnbg2DoKxVikYvC2DamnGBJ";
     const matches = input.match(xmrRegex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal(
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe(
       "42CujFXn1HHiwrGW3Wuh8TASmo94dv3J8DZneS5NqBhaJVNi4qK32Zj3rgcDWsrxznP1qtjJFBKtHQCcbSCY996wMHHfvhw"
     );
   });
@@ -283,15 +281,15 @@ describe("trackerRegex", () => {
   it("should match with all Google Analytics Code values in the input", () => {
     const input = "foo bar UA-26296840-4 baz UA-1111111";
     const matches = input.match(gaTrackIDRegex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("UA-26296840-4");
-    expect(matches[1]).to.equal("UA-1111111");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("UA-26296840-4");
+    expect(matches[1]).toBe("UA-1111111");
   });
   it("should match with all Google Adsense Publisher ID values in the input", () => {
     const input = "foo bar pub-9107453047749393 baz pub-2324633754279327";
     const matches = input.match(gaPubIDRegex)!;
-    expect(matches.length).to.equal(2);
-    expect(matches[0]).to.equal("pub-9107453047749393");
-    expect(matches[1]).to.equal("pub-2324633754279327");
+    expect(matches.length).toBe(2);
+    expect(matches[0]).toBe("pub-9107453047749393");
+    expect(matches[1]).toBe("pub-2324633754279327");
   });
 });
