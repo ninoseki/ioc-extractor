951e7140d4cd9bc5168634f581cfa2a92162ee90
Manabu Niseki <manabu.niseki@gmail.com>
2021-02-12 19:30:30 +0900

    chore: update utility script

--- scripts/tld.rb
+++ scripts/tld.rb
@@ -2,8 +2,18 @@
 
 require "http"
 
-url = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
+def get_latest_tlds
+  url = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
 
-res = HTTP.get(url)
-lines = res.body.to_s.lines
-p lines.reject { |line| line.start_with? "#" }.map(&:chomp).map(&:downcase)
+  res = HTTP.get(url)
+  lines = res.body.to_s.lines
+  lines.reject { |line| line.start_with? "#" }.map(&:chomp).map(&:downcase)
+end
+
+path = File.expand_path("../src/aux/tlds.ts", __dir__)
+data = File.read(path)
+
+latest_tlds = get_latest_tlds
+latest_tlds.each do |tld|
+  p tld unless data.include?(tld)
+end
